f87e89fed1cc195f19361f9b670cb8c2f39085ba
abhishek.c <abhishek.c@chegg.com>
2020-03-30 03:20:15 +0530

    Adding etl process to aggregate country wise data

--- src/main/java/com/corona/insights/dao/CasesDaoImpl.java
+++ src/main/java/com/corona/insights/dao/CasesDaoImpl.java
@@ -21,12 +21,10 @@ public class CasesDaoImpl extends CasesDao {
     }
 
     public void createOrUpdate(Cases cases) {
-
-            DSL.using(configuration()).insertInto(CASES, CASES.LOCATION_ID, CASES.REPORTING_DATE, CASES.CONFIRMED, CASES.DEATHS, CASES.RECOVERED, CASES.REPORTING_TIMESTAMP, CASES.FILE_NAME)
-                    .values(cases.getLocationId(), cases.getReportingDate(), cases.getConfirmed(), cases.getDeaths(), cases.getRecovered(), cases.getReportingTimestamp(), cases.getFileName())
-                    .onDuplicateKeyIgnore()
-                    .execute();
-
+        DSL.using(configuration()).insertInto(CASES, CASES.LOCATION_ID, CASES.REPORTING_DATE, CASES.CONFIRMED, CASES.DEATHS, CASES.RECOVERED, CASES.REPORTING_TIMESTAMP, CASES.FILE_NAME)
+                .values(cases.getLocationId(), cases.getReportingDate(), cases.getConfirmed(), cases.getDeaths(), cases.getRecovered(), cases.getReportingTimestamp(), cases.getFileName())
+                .onDuplicateKeyIgnore()
+                .execute();
     }
 
     //select result.max_reporting_date as "time", sum(result.max_confirmed) as "confirmed cases", sum(result.max_deaths) as "death cases", sum(result.max_recovered) as "recovered cases" from (select max(c.`confirmed`) as max_confirmed, max(c.`deaths`) as max_deaths, max(c.`recovered`) as max_recovered, max(c.`reporting_date`) as max_reporting_date, l.`country` as country from cases c join `location` l on c.`location_id`=l.`id` where l.`country` = "US" group by c.`confirmed`, c.`reporting_date`, c.`location_id`) as result group by time order by time desc;
@@ -34,9 +32,9 @@ public class CasesDaoImpl extends CasesDao {
     public List<CoronaVirusCountryWiseData> aggregateDataForCountry(String country) {
         Table<?> aggregated = DSL.using(configuration()).select(DSL.max(CASES.CONFIRMED).as("aggregated_confirmed"), DSL.max(CASES.DEATHS).as("aggregated_deaths"), DSL.max(CASES.CONFIRMED).as("aggregated_recovered"), DSL.max(CASES.REPORTING_DATE).as("aggregated_reporting_date"))
                                  .from(CASES.join(LOCATION).on(CASES.LOCATION_ID.eq(LOCATION.ID))).where(LOCATION.COUNTRY.eq(country)).groupBy(CASES.CONFIRMED, CASES.REPORTING_DATE, CASES.LOCATION_ID).asTable("aggregated");
-        List<CoronaVirusCountryWiseData> coronaVirusCountryWiseData = DSL.using(configuration())
-                                         .select(aggregated.field("aggregated_reporting_date").as("reportedDate"), DSL.sum(aggregated.field("aggregated_confirmed").coerce(Integer.class)).as("confirmed"), DSL.sum(aggregated.field("aggregated_deaths").coerce(Integer.class)).as("deaths"), DSL.sum(aggregated.field("aggregated_recovered").coerce(Integer.class)).as("recovered")).from(aggregated).groupBy(aggregated.field("aggregated_reporting_date")).orderBy(aggregated.field("aggregated_reporting_date").desc()).fetchInto(CoronaVirusCountryWiseData.class);
-        return coronaVirusCountryWiseData;
+        return DSL.using(configuration()).select(aggregated.field("aggregated_reporting_date").as("reportedDate"), DSL.sum(aggregated.field("aggregated_confirmed").coerce(Integer.class)).as("confirmed"), DSL.sum(aggregated.field("aggregated_deaths").coerce(Integer.class)).as("deaths"), DSL.sum(aggregated.field("aggregated_recovered").coerce(Integer.class)).as("recovered"))
+                                         .from(aggregated).groupBy(aggregated.field("aggregated_reporting_date")).orderBy(aggregated.field("aggregated_reporting_date").desc())
+                                         .fetchInto(CoronaVirusCountryWiseData.class);
     }
 
 }
